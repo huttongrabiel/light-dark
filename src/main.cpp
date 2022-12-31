4fc6f5fb7fc650ad661b1e4df359fc371def1b90
huttongrabiel <huttonthomas@icloud.com>
2022-12-31 10:46:14 -0800

    Add IR transmitter

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <Arduino.h>
 #include <Servo.h>
+#include <IRremote.h>
+
+#define IR_RECV_PIN 4
 
 Servo main_servo;
 
 void setup() {
     main_servo.attach(9);
+    
+    Serial.begin(9600);
+    IrReceiver.begin(IR_RECV_PIN); 
 }
 
 int current_pos = 0;
@@ -12,17 +18,21 @@ int current_pos = 0;
 void loop() {
     current_pos = main_servo.read();
 
-    if (current_pos <= 90) {
-        // Sweep Right
-        for (current_pos = 0; current_pos <= 180; current_pos++) {
-            main_servo.write(current_pos);
-            delay(15);
+    if (IrReceiver.decode()) {
+        Serial.println("Received IR transmission!");
+        if (current_pos <= 90) {
+            // Sweep Right
+            for (current_pos = 0; current_pos <= 180; current_pos++) {
+                main_servo.write(current_pos);
+                delay(15);
+            }
+        } else {
+            // Sweep Left
+            for (current_pos = 180; current_pos >= 0; current_pos--) {
+                main_servo.write(current_pos);
+                delay(15);
+            }
         }
-    } else {
-        // Sweep Left
-        for (current_pos = 180; current_pos >= 0; current_pos--) {
-            main_servo.write(current_pos);
-            delay(15);
-        }
+        IrReceiver.resume();
     }
 }
